ebd4db3307f3e6c47c1e0213da4dcefe210e7819
Eva Rodriguez <eva22rodri@gmail.com>
2019-07-30 19:04:28 -0500

    API Successfully Completed

--- KanyeWestAPI/Program.cs
+++ KanyeWestAPI/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
 
 namespace KanyeWestAPI
 {
@@ -6,7 +8,18 @@ namespace KanyeWestAPI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string url = "https://api.kanye.rest";
+            HttpClient client = new HttpClient();
+            string response = client.GetStringAsync(url).Result;
+
+            // This manual way of pulling out the text of the joke is not good or reliable:
+            //int jokeStartIndex = response.IndexOf("value") + 8;
+            //int jokeLength = response.Length - jokeStartIndex - 2;
+            //string joke = response.Substring(jokeStartIndex, jokeLength
+
+            string joke2 = JObject.Parse(response).GetValue("quote").ToString();
+
+            Console.WriteLine(joke2);
         }
     }
 }
